df42a4a84f81c236559f0668158cef91ed677636
prare11 <prare1107@gmail.com>
2024-07-30 18:34:01 +0300

    [ADD] practice 1.2

---
+++ Project1.2.py
@@ -0,0 +1,3 @@
+pet_kind: str = input("Please enter kind of your pet -> \n")
+pet_name: str = input("Please enter your pet name -> \n ")
+print("My " + pet_kind + "  is a beautiful animal. Its name is " + pet_name)
