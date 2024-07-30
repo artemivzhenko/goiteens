c98f22656c36b0725ae0e679860bca9cf6fc3ffb
prare11 <prare1107@gmail.com>
2024-07-30 18:27:05 +0300

    [ADD] first practic lesson

---
+++ Project1.py
@@ -0,0 +1,3 @@
+actor_name: str = input("Please enter your favourite actor name -> \n")
+film_name: str = input("Please enter your favourite film with that actor -> \n ")
+print("This beautiful actor - " + actor_name + " was filmed in this film " + film_name)
