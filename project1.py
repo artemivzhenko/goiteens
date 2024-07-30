a8c6c973f961271167835a885f9620d3edbcec39
Andrii446 <andr228ts@gmail.com>
2024-07-30 17:55:45 +0200

    [add] project1

---
+++ project1.py
@@ -0,0 +1,3 @@
+actor: str = input("who is your favorite actor \n")
+actor1: str = input("what is your favorite movie where this actor was shot \n")
+print("This beautiful actor -" + actor + "was filmed in this film " + actor1)
