1a2d0709a4bdd3e5ff884e228c92fed4b9b731ef
Andrii446 <andr228ts@gmail.com>
2024-07-30 17:39:04 +0200

    [add] first project

---
+++ project.py
@@ -0,0 +1,8 @@
+def get_user():
+    hello_message: str = "Hello"
+    print(hello_message + " World!")
+    user_name: str = input("Please put your name -> \n")
+    print(hello_message + " " + user_name)
+    user_age_str: str = input("How old are you -> \n")
+    user_age: int = int(user_age_str)
+    print(user_name + " is " + str(user_age) + " years old")
