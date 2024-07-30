30d6a703369cd08d4f1b117062216042e0704e6b
eskton <antonatherton@gmail.com>
2024-07-30 18:25:42 +0300

    [add] add first homework

---
+++ homework1.py
@@ -0,0 +1,13 @@
+hello_message: str = "Hello"
+print(hello_message + " World!")
+user_name: str = input("Please put your name -> \n")
+print(hello_message + " " + user_name)
+user_age_str: str = input("How old are you -> \n")
+user_age: int = int(user_age_str)
+print(user_name + " is " + str(user_age) + " years old")
+user_country: str = input("Please put your country -> \n")
+print(hello_message + " " + user_country)
+height = float(input("Please enter your height in centimeters: "))
+
+
+
