4604f644044b2dfa79260c43fb9f054a85b663c9
Arina288 <a18770@agrupamento-eugenioandrade.org>
2024-07-30 18:42:20 +0300

    [add]

---
+++ main.py
@@ -0,0 +1,12 @@
+def get_user
+hello_message: str = "Hello"
+print(hello_message + "World!")
+user_name: str = input ("Please put your name -> \n")
+print(hello_message + " " + user_name)
+user_age_str: str = int( "How old are you -> \n")
+user_age: int = int(user_age_str)
+print(user_name + " is " + str(user_age)
+country: str = input("Country of user")
+print("user live in " + country)
+city("user live in" + city)
+print("user live in" + city)
