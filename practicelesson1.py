2eba2dfc76526c634db3f92367f4a376e30ea6be
eskton <antonatherton@gmail.com>
2024-07-30 18:58:32 +0300

    [add] practice

---
+++ practicelesson1.py
@@ -0,0 +1,10 @@
+actor = input("Please enter your favorite actor: ")
+
+# Запит у користувача улюбленого фільму, в якому знімався цей актор
+movie = input(f"Please enter your favorite movie that {actor} starred in: ")
+
+# Виведення повідомлення на екран
+print(f"This beautiful actor - {actor} was filmed in this film - {movie}.")
+
+city = input("Please enter the {city} where you live ")
+print(f"{city} is a wonderful place!")
