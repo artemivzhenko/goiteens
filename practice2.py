cea6153f7053387a76511a16c905a770694bf1bc
Andrii446 <andr228ts@gmail.com>
2024-07-31 22:24:35 +0200

    [add] practice work

---
+++ practice2.py
@@ -0,0 +1,2 @@
+user_city: str = input("Type the city ->")
+print(user_city + " is a wonderful place!")
---
+++ practice3.py
@@ -0,0 +1,3 @@
+animal: str = input("What animal do you have ->")
+animal_name: str = input("What is your pet's name ->")
+print("My " + animal + " is a beautiful animal. It`s name is " + animal_name)
