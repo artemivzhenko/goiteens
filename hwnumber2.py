cc6f4eeb5926e414b7ff45d3c0c7763e05730426
eskton <antonatherton@gmail.com>
2024-08-01 19:00:19 +0300

    [add] hwnum2

---
+++ hwnumber2.py
@@ -0,0 +1,24 @@
+actor = input("Please enter your favorite actor: ")
+
+# Запит у користувача улюбленого фільму, в якому знімався цей актор
+movie = input(f"Please enter your favorite movie that {actor} starred in: ")
+
+# Виведення повідомлення на екран
+print(f"This beautiful actor - {actor} was filmed in this film - {movie}.")
+
+city = input("Please enter the {city} where you live ")
+print(f"{city} is a wonderful place!")
+
+
+animal = input("Please enter the type of animal you have: ")
+name = input(f"Please enter the name of your {animal}: ")
+print(f"My {animal} is a beautiful animal. Its name is {name}.")
+
+
+print(f"*******")
+print(f"******")
+print(f"*****")
+print(f"****")
+print(f"***")
+print(f"**")
+print(f"*")
