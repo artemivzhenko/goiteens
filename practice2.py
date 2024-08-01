e2fe5987b850254cb55a49d7fe098f192782de49
Artem Ivzhenko <109346326+artemivzhenko@users.noreply.github.com>
2024-08-01 18:13:22 +0300

    [ADD] homework num2

--- practice2.py
+++ practice2.py
@@ -1,2 +1,6 @@
+# Напишіть програму, яка просить користувача ввести місто, у якому він живе.
+# Виведіть назву міста разом із фразою «is a wonderful place!».
+# Наприклад: Kyiv is a wonderful place!
+
 user_city: str = input("Type the city ->")
 print(user_city + " is a wonderful place!")
