1b22814e22144d996db018f0b28a788426f7ed51
Arina288 <a18770@agrupamento-eugenioandrade.org>
2024-08-07 11:26:44 +0300

    [ADD] practic
    ...

---
+++ lesson....py
@@ -0,0 +1,26 @@
+my_first_list: list = ["Artem",10, True, 0.12]
+
+
+first_star: str = ""
+
+for number in range(1,6)
+    first_star += ""
+
+for number in range(1,6):
+    first_star += "*"
+    print(first_star)
+
+
+
+number = int("Введіть число:"))
+if number >= 100:
+    print("Ви ввели число:", number)
+else:
+    print("Число менше 100. Будь ласка, спробуйте ще раз")
+
+
+my_first_list =["monday","tuesday","wednesday","thursday","saturday","sunday"]
+for number in range(5)
+    user_number = input("put your number")
+    user_number_int= input("put your number")
+    print(my_first_list[user_number_int+1])
