131e88ce0e4f62fda985b53380bb5dff568069ea
Artem Ivzhenko <109346326+artemivzhenko@users.noreply.github.com>
2024-08-13 18:00:19 +0300

    [REF]

--- basic_types/number_type.py
+++ basic_types/number_type.py
@@ -1,20 +1,20 @@
 # INTEGER, FLOAT
 
-x: float = 13.56
-y: float = -5.1
+x: int = 14
+y: int = -5
 
-print("X * Y = " + str(x * y))
-print("X + Y = " + str(x + y))
-print("X - Y = " + str(x - y))
-print("X / Y = " + str(x / y))
-print("X ** Y = " + str(x ** -y))
-print("X // Y = " + str(x // y))
-print("X % Y = " + str(x % y))
-str_x: str = str(x)
-str_y: str = str(y)
+print("X * Y = " + str(x * y))  # Множення
+print("X + Y = " + str(x + y))  # Додавання
+print("X - Y = " + str(x - y))  # Віднімання
+print("X / Y = " + str(x / y))  # Ділення
+print("X ** Y = " + str(x ** -y))  # Взведення у степінь
+print("X // Y = " + str(x // y))  # Ділення без остачі
+print("X % Y = " + str(x % y))  # Отримання остачі від ділення
+str_x: str = str(x)  # Перетвоерння числа у текст
+str_y: str = str(y)  # Перетвоерння числа у текст
 
-x_int: int = int(float(str_x))
-y_int: int = int(float(str_y))
+x_int: int = int(float(str_x))  # Перетвоерння тексту у число
+y_int: int = int(float(str_y))  # Перетвоерння тексту у число
 print(x_int)
 print(y_int)
 
