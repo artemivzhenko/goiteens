c305638eebb3cb234d400d63ae1580392e361670
MoRpheUSx777 <130pmcy@gmail.com>
2024-08-15 18:13:06 +0200

    [ADD] add lesson 7.1

---
+++ practice/lesson 7.1.py
@@ -0,0 +1,4 @@
+user_input= int(input("put your number\n"))
+result = "Число кратне 5 і більше 100" if user_input ** 5 >= 100 else "Число не кратне 5 і меньше 100"
+
+print(result)
--- practice/lesson 7.py
+++
@@ -1,6 +0,0 @@
-def check_number_in_range(number):
-    result = "Число кратне 5 і більше 100" if number**5 >= 100 else "Число не кратне 5 і меньше 100”."
-    return result
-
-input_number = int(input("Введіть ціле число: "))
-print(check_number_in_range(input_number))
