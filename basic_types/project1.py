f049eb9cf46d410eddf6a6937049d8286e33e80c
Artem Ivzhenko <109346326+artemivzhenko@users.noreply.github.com>
2024-08-15 19:04:53 +0300

    [REF]

--- basic_types/project1.py
+++
@@ -1,3 +0,0 @@
-actor: str = input("who is your favorite actor \n")
-actor1: str = input("what is your favorite movie where this actor was shot \n")
-print("This beautiful actor -" + actor + "was filmed in this film " + actor1)
