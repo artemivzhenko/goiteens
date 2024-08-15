eb5698e0c98215a5e0693bd43bc95fe88ebcbbbd
MoRpheUSx777 <130pmcy@gmail.com>
2024-08-15 18:07:05 +0200

    [ADD]  ADD work

--- practice/lesson 7.py
+++ practice/lesson 7.py
@@ -1,27 +1,6 @@
-"""
-user_value = int(input("please put your number\n"))
-value = user_value ** 2 if user_value < 100 else user_value / 2
-print(value)
-x=input("please put x\n")
-x=int(x)
-y=input("please put y\n")
-y=int(y)
-message ="x bigger than y" if x>y else print("y bigger than x")
-"""
-"""
-user_value=int(input("please put your number\n"))
-value = user_value + 1 if user_value >=0 else user_value-2
-print(value)
-"""
-"""
-user_value =input("please put your number\n")
-message ="число парное " if int(user_value)%2==0 else "число не парное "
-value_len=len(user_value)
-if value_len==1:
-    message+="однозначное"
-elif value_len==2:
-    message+="двухзначное"
-elif value_len==3:
-    message+="трёхзначное"
-print(message)
-"""
+def check_number_in_range(number):
+    result = "Число кратне 5 і більше 100" if number**5 >= 100 else "Число не кратне 5 і меньше 100”."
+    return result
+
+input_number = int(input("Введіть ціле число: "))
+print(check_number_in_range(input_number))
