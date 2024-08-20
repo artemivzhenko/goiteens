f8da870cf90f203d826ac15c51c22135c38c9726
eskton <antonatherton@gmail.com>
2024-08-20 19:29:31 +0300

    (ADD)

--- pr2.py
+++ pr2.py
@@ -1,7 +1,24 @@
-my_first_list=["monday","tuesday","wednesday","thursday","friday","sunday","saturday"]
-for number in range(1, 6):
-    user_number = int(input("Введіть число: "))
-    print(my_first_list[user_number])
-#print(my_first_list[])
+#user_input = input("Put your string:").split(",")
+
+#print (user_input)
+#number = int(user_input[1])
+#user_string = user_input[0]
+#print(number * user_string)
+
+
+
+
+
+
+#user_value = int(input("Please put number\n"))
+#value = user_value ** 2 if user_value < 100 else user_value / 2
+#print(value)
+x = input("Please put x\n")
+x = int(x)
+y = input("Please put y\n")
+y = int(y)
+message = "x > y" if x>y else "y > x"
+print(message)
+
 
 
---
+++ practice/practice2.2.py
@@ -0,0 +1,13 @@
+list_1 = [-2, -4, -6, 2, 5]
+list_2 = [3, 4, 6, 7, 6, -4, 1, -6, 5, -7]
+
+list_3 = [list_1, list_2]
+x = 0
+y = 0
+for my_list in list_3:
+    for my_item in my_list:
+        if my_item >0:
+             x += my_item
+else:
+             y -= my_item
+             print(x,y)
