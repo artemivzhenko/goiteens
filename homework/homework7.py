d7a82278746fe775d45cfcd448cfdcc69777ef71
prare11 <prare1107@gmail.com>
2024-08-22 18:45:28 +0300

    [ADD] add homework7

---
+++ homework/homework7.py
@@ -0,0 +1,7 @@
+first_list = [2, 4, 6, 8, 10]
+second_list = [2, 4, 6, 8, 10]
+
+for first_number in first_list:
+    for second_number in second_list:
+        print(f"{first_number} x {second_number} = {first_number * second_number}")
+    print()
