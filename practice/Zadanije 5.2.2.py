5d2f4ce4e6eaa70adaf04d9427b28f5cf15a170b
Pustelga-1 <k35719659@gmail.com>
2024-08-22 17:49:59 +0200

    Create Zadanije 5.2.2.py
    Практическое задание №2 урока № 5.2

---
+++ practice/Zadanije 5.2.2.py
@@ -0,0 +1,9 @@
+def print_inverted_triangle(n):
+    for i in range(n, 0, -1):
+        for j in range(1, i+1):
+            print(j, end=" ")
+        print()
+
+n = int(input("Введите высоту триугольника: "))
+
+print_inverted_triangle(n)
