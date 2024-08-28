e501ba01bceb53cafc117bbd2f7891c4e595bd43
Arina288 <a18770@agrupamento-eugenioandrade.org>
2024-08-28 12:14:11 +0300

    [add] practic

---
+++ practice/Practic 27.08.py
@@ -0,0 +1,19 @@
+N = int(input("Enter number: "))
+sum_even = 0
+print("The sum of all even numbers from 1 to "N" is equal", sum_even"")
+
+
+
+
+number = int(input("Enter a number: "))
+if number > 100:
+print("The entered number is greater than 100. The cycle stops.")
+result = number * 4
+print("The result of multiplying by 4:", result)
+
+
+
+
+list_number_one = ["Dima", "Yaroslav", "Zhenya", "Sasha", "Andrii", "Max"]
+for name in list_number_one:
+print(f"Hello {name}!")
