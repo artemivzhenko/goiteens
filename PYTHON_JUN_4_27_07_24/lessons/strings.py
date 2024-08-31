ccb28f51403de0d5f3f01143f53ab99fef1cf9d2
Artem Ivzhenko <109346326+artemivzhenko@users.noreply.github.com>
2024-08-31 19:53:45 +0300

    [ADD]

---
+++ PYTHON_JUN_4_27_07_24/lessons/strings.py
@@ -0,0 +1,34 @@
+# my_string_value = "Hello World!"
+# my_string_value += "!!!!!!!"  # Аналогічно my_string_value = my_string_value + "!!!!!!!"
+#
+# my_string_value *= 7
+#
+# print(my_string_value)
+#
+# my_string_value = "Hello\nWorld!"
+# print(my_string_value)
+#
+# my_long_string_value = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+# Donec sit amet lacus lorem. Pellentesque iaculis dui est,
+# quis facilisis tellus convallis ut.
+# Vestibulum molestie at enim id ornare.
+# Duis turpis magna, feugiat sit amet lectus non,
+# rhoncus euismod metus. Duis consectetur vestibulum consectetur.
+# Nullam mattis diam tincidunt dignissim feugiat.
+# Praesent urna ligula, ornare et scelerisque eu, ornare eget ex.
+# Nunc viverra molestie dui.
+# """
+# print(my_long_string_value)
+
+
+my_long_string_value = "Hello World!"
+print(my_long_string_value.lower())
+print(my_long_string_value.upper())
+print(my_long_string_value.capitalize())
+print(my_long_string_value.count("o"))
+
+my_integer_value = 40
+message = f"My integer value is {my_integer_value + 20} !!!"
+print(message)
+
+
