8af4a15b106639615c1e9e909c3feef4049ae748
mas1k-py <mlakhotskij11@gmail.com>
2024-09-07 13:43:44 -0400

    [add]Homework4

--- PYTHON_JUN_4_27_07_24/homework/homework_4.py
+++ PYTHON_JUN_4_27_07_24/homework/homework_4.py
@@ -24,28 +24,28 @@ screen.mainloop()
 # Напишіть програму, яка виводить рядок "Hello, World!" у трьох різних шрифтах та кольорах за допомогою Turtle.
 
 
-import turtle
-
-
-screen = turtle.Screen()
-pen = turtle.Turtle()
-
-
-text = "Hello World!"
-
-pen.color("Red")
-pen.write(text, font=("Arial", 24, "normal"))
-
-pen.up()
-pen.goto(0, 100)
-pen.down()
-pen.color("Blue")
-pen.write(text, font=("Comic Sans MS", 24, "normal"))
-
-pen.up()
-pen.goto(0, 200)
-pen.down()
-pen.color("Black")
-pen.write(text, font=("Verdana", 24, "normal"))
-
-screen.mainloop()
+# import turtle
+#
+#
+# screen = turtle.Screen()
+# pen = turtle.Turtle()
+#
+#
+# text = "Hello World!"
+#
+# pen.color("Red")
+# pen.write(text, font=("Arial", 24, "normal"))
+#
+# pen.up()
+# pen.goto(0, 100)
+# pen.down()
+# pen.color("Blue")
+# pen.write(text, font=("Comic Sans MS", 24, "normal"))
+#
+# pen.up()
+# pen.goto(0, 200)
+# pen.down()
+# pen.color("Black")
+# pen.write(text, font=("Verdana", 24, "normal"))
+#
+# screen.mainloop()
