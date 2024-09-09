fc1f3a62219ffe01de13e1f450e190108fa52276
Pustelga-1 <k35719659@gmail.com>
2024-09-09 09:20:58 +0200

    Update DZ № 7.2.1.py
    Обновленное задание №1 урока №7.2

--- homework/DZ № 7.2.1.py
+++ homework/DZ № 7.2.1.py
@@ -1,7 +1,11 @@
+from collections import Counter
+
 def count_unique_elements(lst):
-    unique_elements = set(lst)
+    element_counts = Counter(lst)
+    unique_elements = [elem for elem, count in element_counts.items() if count == 1]
     return len(unique_elements)
 
-my_list = [1, 2, 2, 3, 4, 4, 5, 5, 5]
-result = count_unique_elements(my_list)
-print("Количество уникальних елементов:", result)
+lst = [1, 2, 3, 4, 2, 3, 5, 6, 7, 8, 8, 9, 10]
+unique_count = count_unique_elements(lst)
+print(f"Количество уникальных элементов: {unique_count}")
+
