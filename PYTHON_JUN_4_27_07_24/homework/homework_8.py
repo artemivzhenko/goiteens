625247aa7b865bd59f6bb52b73902148e028a047
RoLToNn1 <nazar.marchenko11@gmail.com>
2024-10-13 16:21:51 +0300

    add homework_8.py

---
+++ PYTHON_JUN_4_27_07_24/homework/homework_8.py
@@ -0,0 +1,157 @@
+import turtle
+
+
+t = turtle.Turtle()
+screen = turtle.Screen()
+
+user_input = int(screen.textinput("Figure","Put number(4-6) --> \n"))
+
+
+if user_input == 4:
+    t.up()
+    t.goto(330, 245)
+    t.down()
+    for _ in range(8):
+        t.circle(30, 45)
+        t.stamp()
+
+
+    t.up()
+    t.goto(195, 145)
+    t.down()
+    for _ in range(4):
+        t.forward(25)
+        t.stamp()
+        t.forward(25)
+        t.stamp()
+        t.left(90)
+
+
+    t.up()
+    t.goto(55, 45)
+    t.down()
+    for _ in range(3):
+        t.forward(25)
+        t.stamp()
+        t.forward(25)
+        t.stamp()
+        t.left(120)
+
+
+    t.up()
+    t.goto(-90, -45)
+    t.down()
+    for _ in range(5):
+        t.forward(20)
+        t.stamp()
+        t.forward(20)
+        t.stamp()
+        t.left(72)
+
+
+elif user_input == 5:
+    t.up()
+    t.goto(330, 245)
+    t.down()
+    for _ in range(8):
+        t.circle(30, 45)
+        t.stamp()
+
+
+    t.up()
+    t.goto(195, 145)
+    t.down()
+    for _ in range(4):
+        t.forward(25)
+        t.stamp()
+        t.forward(25)
+        t.stamp()
+        t.left(90)
+
+
+    t.up()
+    t.goto(55, 45)
+    t.down()
+    for _ in range(3):
+        t.forward(25)
+        t.stamp()
+        t.forward(25)
+        t.stamp()
+        t.left(120)
+
+
+    t.up()
+    t.goto(-90, -45)
+    t.down()
+    for _ in range(5):
+        t.forward(20)
+        t.stamp()
+        t.forward(20)
+        t.stamp()
+        t.left(72)
+
+    t.up()
+    t.goto(-240, -145)
+    t.down()
+    for _ in range(8):
+        t.circle(30, 45)
+        t.stamp()
+
+
+elif user_input == 6:
+    t.up()
+    t.goto(330, 245)
+    t.down()
+    for _ in range(8):
+        t.circle(30, 45)
+        t.stamp()
+
+    t.up()
+    t.goto(195, 145)
+    t.down()
+    for _ in range(4):
+        t.forward(25)
+        t.stamp()
+        t.forward(25)
+        t.stamp()
+        t.left(90)
+
+    t.up()
+    t.goto(60, 45)
+    t.down()
+    for _ in range(3):
+        t.forward(25)
+        t.stamp()
+        t.forward(25)
+        t.stamp()
+        t.left(120)
+
+    t.up()
+    t.goto(-75, -45)
+    t.down()
+    for _ in range(5):
+        t.forward(25)
+        t.stamp()
+        t.forward(25)
+        t.stamp()
+        t.left(72)
+
+    t.up()
+    t.goto(-210, -145)
+    t.down()
+    for _ in range(8):
+        t.circle(30, 45)
+        t.stamp()
+
+
+    t.up()
+    t.goto(-330, -245)
+    t.down()
+    for _ in range(4):
+        t.forward(25)
+        t.stamp()
+        t.forward(25)
+        t.stamp()
+        t.left(90)
+
+screen.mainloop()
