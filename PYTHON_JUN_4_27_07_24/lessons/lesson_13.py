ee314d2fce657f00e9a6a58b0808149c12d2edb6
Artem Ivzhenko <109346326+artemivzhenko@users.noreply.github.com>
2024-11-02 19:50:26 +0200

    [ADD] lesson 13

--- PYTHON_JUN_4_27_07_24/lessons/lesson_13.py
+++ PYTHON_JUN_4_27_07_24/lessons/lesson_13.py
@@ -1,5 +1,44 @@
 # Type list
-my_list = ['Artem', 'Nazar', 'Maryan', 'Svitlana', 'Andriy', 'Artur', "111"]
+
+my_value = "Kiril"
+
+my_list = ['Artem', 'Nazar', 'Maryan', 'Svitlana', my_value]
+
+my_second_list = ['Andriy', 'Artur', "Ivan"]
+
+my_third_list = my_list + my_second_list
+print(my_third_list)
+
+my_list.extend(my_second_list)
+print(my_list)
+
+print(my_list)
+
+my_list[2] = "Kiril"
+#my_list.clear()
+#print(my_list)
+
+my_copy_list = my_list.copy()
+print(my_copy_list)
+
+my_list_comprehension = [name + " Smith" for name in my_list]
+print(my_list_comprehension)
+
+my_list_comprehension = [name + " Smith"if "A" in name else name + " Anderson" for name in my_list]
+print(my_list_comprehension)
+
+# List to tuple
+my_list_tuple = tuple(my_list_comprehension)
+# List to set
+my_list_set = set(my_list_comprehension)
+
+# List to dictionary
+my_dict_list = dict.fromkeys(my_list_comprehension, "Ukraine")
+
+print(my_dict_list)
+
+
+
 #             0        1         2          3          4         5     6   7   8    9     10     11     12
 #           -13       -12       -11        -10         -9       -8    -7  -6  -5   -4     -3     -2     -1
 import random
@@ -13,24 +52,24 @@ print(my_list[random_index])
 print(my_list[1:6:2])
 
 print(f"Повертає розмірність списку {len(my_list)}")
-# print(my_list.index('Maryan'))
-# my_list.sort()
-# print(my_list)
-#
-# my_list_2 = my_list[::-1]
-# print(my_list_2)
-#
-#
-# my_list.reverse()
-# print(my_list)
-#
-#
-# for item in my_list:
-#     print(f"Item in my list {item}")
+print(my_list.index('Maryan'))
+my_list.sort()
+print(my_list)
+
+my_list_2 = my_list[::-1]
+print(my_list_2)
+
+
+my_list.reverse()
+print(my_list)
+
+
+for item in my_list:
+    print(f"Item in my list {item}")
 
 my_list.append("Ivan")
 print(my_list)
-my_list.insert(3, "Dmitriy")
+my_list.insert(0, "Dmitriy")
 print(my_list)
 
 my_list.remove("Ivan")
@@ -46,7 +85,7 @@ print(my_list)
 my_list[2] = "Kiril"
 print(my_list)
 
-#Type tuple
+# Type tuple
 
 my_tuple = ('Artem', 'Nazar', 'Maryan')
 print(my_tuple[2])
@@ -87,3 +126,13 @@ for value in my_dictionary.values():
 for key, value in my_dictionary.items():
     print(key, value)
 
+
+import random
+
+list_1 = []
+
+for i in range(10):
+    list_1.append(random.randint(15, 20))
+
+print(max(list_1))
+print(min(list_1))
